274b3c32c7e642c38f59a07b4b2fb5a5c26289c7
Serge Pirogov <d3ljke@gmail.com>
2019-04-30 02:07:58 +0200

    added mage attack animation

--- Units/Mage/Mage.gd
+++ Units/Mage/Mage.gd
@@ -1,15 +1,39 @@
 extends "../../Unit.gd"
 
 export (int) var damage := 2
+export (PackedScene) var AOE
 
 func tick(_allies: Array, _enemies: Array):
     pass
 
+func _ready():
+    var game = get_node("/root/Game")
+    game.connect("buy_stage", self, "_on_Game_buy_stage")
+    game.connect("fight_stage", self, "_on_Game_fight_stage")
+
+func _on_Game_buy_stage():
+    $CastSpeed.stop()
+    
+func _on_Game_fight_stage():
+    $CastSpeed.start()
 
 func _on_CastSpeed_timeout():
+    $AnimatedSprite.play("damage")
+
+func _on_AnimatedSprite_animation_finished():
+    $AnimatedSprite.stop()
+    $AnimatedSprite.frame = 0
     var enemies = get_enemies()
     if enemies.size() > 0:
         var enemy = search_nearest(enemies)
-        var area = search_nearby(enemy, enemies)
-        for _enemy in area:
-            _enemy.take_damage(damage)
+        cast_aoe(enemy, damage)
+            
+func cast_aoe(unit: Unit, damage: int):
+    var aoe = AOE.instance()
+    aoe.position = unit.position
+    get_node("/root/Game").add_child(aoe)
+    
+    var enemies = get_enemies()
+    var area = search_nearby(unit, enemies)
+    for enemy in area:
+        enemy.take_damage(damage)
