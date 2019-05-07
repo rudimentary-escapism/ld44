5ea61464620bf31007ebfc2ad3a1aaab8808db42
Serge Pirogov <d3ljke@gmail.com>
2019-05-07 23:03:22 +0200

    intro strings show line per click

--- Intro.gd
+++ Intro.gd
@@ -1,8 +1,28 @@
 extends Control
 
+var state := 1
+
 func _ready():
     $Hat/AnimationPlayer.play("New Anim")
     
 func _input(event):
     if event is InputEventMouseButton:
-        get_tree().change_scene("res://Game.tscn")
+        if event.is_pressed():
+            mouse_clicked()
+        
+func mouse_clicked():
+    match state:
+        0:
+            $UIText/UIText.text =\
+"Cookie Commander, we were attacked!"
+        1:
+            $UIText/UIText.text =\
+"""You need to build army but won't forget:
+to make soldiers you need to sacrifice
+your own pieces."""
+        2:
+            $UIText/UIText.text =\
+"Don't waste yourself totally!\nGood luck, Commander!"
+        _:
+            get_tree().change_scene("res://Game.tscn")
+    state += 1
