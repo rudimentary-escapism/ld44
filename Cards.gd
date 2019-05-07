923064f3d028f24de502f20f1053d4849d10291a
Serge Pirogov <d3ljke@gmail.com>
2019-05-07 23:20:27 +0200

    added images and description to cards

--- Cards.gd
+++ Cards.gd
@@ -11,6 +11,13 @@ var archer = preload("res://Units/Archer/Archer.tscn")
 var rogue = preload("res://Units/Rogue/Rogue.tscn")
 var mage = preload("res://Units/Mage/Mage.tscn")
 
+var warrior_t = preload("res://Units/Warrior/Warrior.png")
+var paladin_t = preload("res://Units/Paladin/Paladin.png")
+var priest_t = preload("res://Units/Priest/Priest.png")
+var archer_t = preload("res://Units/Archer/Archer.png")
+var rogue_t = preload("res://Units/Rogue/Rogue.png")
+var mage_t = preload("res://Units/Mage/Mage.png")
+
 func _ready():
     generate()
 
@@ -21,21 +28,46 @@ func generate():
             0:
                 card.unit = warrior
                 card.get_node("Button/Label").text = "Warrior"
+                card.get_node("Button/Image").texture = warrior_t
+                card.get_node("Button/Description").text =\
+"""- Melee
+- High hp"""
             1:
                 card.unit = paladin
                 card.get_node("Button/Label").text = "Paladin"
+                card.get_node("Button/Image").texture = paladin_t
+                card.get_node("Button/Description").text =\
+"""- Melee
+- Weak heal"""
             2:
                 card.unit = priest
-                card.get_node("Button/Label").text = "Priest"  
+                card.get_node("Button/Label").text = "Priest" 
+                card.get_node("Button/Image").texture = priest_t 
+                card.get_node("Button/Description").text =\
+"""- No attack
+- Strong heal"""
             3:
                 card.unit = archer
-                card.get_node("Button/Label").text = "Archer"   
+                card.get_node("Button/Label").text = "Archer"
+                card.get_node("Button/Image").texture = archer_t 
+                card.get_node("Button/Description").text =\
+"""- Range"""  
             4:
                 card.unit = rogue
-                card.get_node("Button/Label").text = "Rogue"  
+                card.get_node("Button/Label").text = "Rogue" 
+                card.get_node("Button/Image").texture = rogue_t 
+                card.get_node("Button/Description").text =\
+"""- Melee
+- Attack
+  weak
+  target"""
             5:
                 card.unit = mage
                 card.get_node("Button/Label").text = "Mage"
+                card.get_node("Button/Image").texture = mage_t
+                card.get_node("Button/Description").text =\
+"""- Range
+- AOE"""
         card.show()
 
 func _on_Card_pressed(unit):
